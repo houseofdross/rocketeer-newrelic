f3fc00f7a801b282d1a605e01679ab1cde3c2267
Matthew Hallsworth <matthew.hallsworth@peakadventuretravel.com>
2015-11-12 15:12:05 +1100

    Add more config options for flexibility

--- src/config/config.php
+++ src/config/config.php
@@ -3,13 +3,14 @@
 return [
 
     // Define the parameters of your installation here
-    // Can have more than one application ID as one deployment may cover many applications
 
-    'apiKey'        => '',
-    'applicationId' => [
+
+    'environment'   => 'prd',  // Which environment to deploy in
+    'apiKey'        => '',     // Insert your api key to talk to newrelic
+    'applicationIds' => [      // Can have more than one application ID as one deployment may cover many applications
         '',
     ],
-    'user'          => null,
+    'user'          => null,   // Optional params to tell newrelic more info
     'description'   => null,
     'revision'      => null,
 
