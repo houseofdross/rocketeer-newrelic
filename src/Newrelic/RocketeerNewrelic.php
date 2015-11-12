0b33d193081f43f93140ab39ebf58af5cc14196f
Matthew Hallsworth <matthew.hallsworth@peakadventuretravel.com>
2015-11-12 15:38:43 +1100

    Finish up buggy fixes and improve doco

--- src/Newrelic/RocketeerNewrelic.php
+++ src/Newrelic/RocketeerNewrelic.php
@@ -43,7 +43,7 @@ class RocketeerNewrelic extends AbstractPlugin
     public function onQueue(TasksHandler $queue)
     {
         $queue->after('deploy', function ($task) {
-            $this->afterDeploy();
+            $this->afterDeploy($task);
         });
     }
 
@@ -52,19 +52,24 @@ class RocketeerNewrelic extends AbstractPlugin
      *
      * @throws \Exception
      */
-    private function afterDeploy()
+    private function afterDeploy($task)
     {
         $config = $this->app['newrelic']->getConfig();
+        $environment = $task->getOption('on');
 
-        $apiKey = $config['apiKey'];
-        $applicationIdCollection = $config['applicationId'];
-        $user = $config['user'];
-        $description = $config['description'];
-        $revision = $config['revision'];
+        // Only deploy when the environment tag is set and matches the --on flag given
+        if ($environment != NULL && $environment == $config['environment']) {
+            $apiKey = $config['apiKey'];
+            $applicationIdCollection = $config['applicationIds'];
+            $user = $config['user'];
+            $description = $config['description'];
+            $revision = $config['revision'];
 
-        foreach ($applicationIdCollection as $applicationId) {
-            $service = new RocketeerNewrelicDeploymentService($apiKey);
-            $service->notify($applicationId, $user, $description, $revision);
+            foreach ($applicationIdCollection as $applicationId) {
+                $service = new RocketeerNewrelicDeploymentService($apiKey);
+                $service->notify($applicationId, $user, $description, $revision);
+                $task->command->info('Newrelic notified for app: '.$applicationId);
+            }
         }
     }
 }
